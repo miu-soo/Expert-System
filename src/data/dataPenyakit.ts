657723d2864df11a101e8d7c4ba0d9c38b69d0fe
CMMhero <ch.mh.marvelio@gmail.com>
2022-11-21 11:33:14 +0700

    test

--- src/data/dataPenyakit.ts
+++ src/data/dataPenyakit.ts
@@ -14,22 +14,19 @@ const Penyakit = [
   {
     id: 3,
     name: "Arrhythmia",
-    description:
-      "Improper beating of the heart, whether irregular, too fast or too slow.",
+    description: "Improper beating of the heart, whether irregular, too fast or too slow.",
     detail: "Detail penyakit 1",
   },
   {
     id: 4,
     name: "Penyakit katup jantung",
-    description:
-      "One or more of the valves in your heart doesn't work properly",
+    description: "One or more of the valves in your heart doesn't work properly",
     detail: "Detail penyakit 1",
   },
   {
     id: 5,
     name: "Gagal Jantung",
-    description:
-      "A chronic condition in which the heart doesn't pump blood as well as it should.",
+    description: "A chronic condition in which the heart doesn't pump blood as well as it should.",
     detail: "Detail penyakit 1",
   },
 ];
