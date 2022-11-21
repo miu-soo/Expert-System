d55ee649e602cbe4b654a918ac38e51325068ebf
miu <ipod.sulistyo@gmail.com>
2022-11-21 10:24:52 +0700

    Feat: update KB

--- src/data/dataGejala.ts
+++ src/data/dataGejala.ts
@@ -1,38 +1,158 @@
 const Gejala = [
   {
     id: 1,
-    name: "Flu",
+    name: "Sesak Dada",
     value: 1,
   },
   {
     id: 2,
-    name: "Flu2",
+    name: "Kehabisan Napas",
     value: 2,
   },
   {
     id: 3,
-    name: "Flu3",
+    name: "Bahu kiri terasa tidak enak",
     value: 3,
   },
   {
     id: 4,
-    name: "Flu4",
+    name: "Keringat Dingin",
     value: 4,
   },
   {
     id: 5,
-    name: "Flu5",
+    name: "Dada terasa sakit selama 15 menit lebih",
     value: 5,
   },
   {
     id: 6,
-    name: "Flu6",
+    name: "Ischemia (kurangnya oksigen)",
     value: 6,
   },
   {
     id: 7,
-    name: "Flu7",
+    name: "Detak jantung tidak teratur",
     value: 7,
+  },
+  {
+    id: 8,
+    name: "Merasa resah",
+    value: 8,
+  },
+  {
+    id: 9,
+    name: "Sering pingsan",
+    value: 9,
+  },
+  {
+    id: 10,
+    name: "Pusing kepala",
+    value: 10,
+  },
+  {
+    id: 11,
+    name: "Detak jantung terasa keras",
+    value: 11,
+  },
+  {
+    id: 12,
+    name: "Lemas",
+    value: 12,
+  },
+  {
+    id: 13,
+    name: "Punggung terasa tidak enak",
+    value: 13,
+  },
+  {
+    id: 14,
+    name: "Badan terasa lelah",
+    value: 14,
+  },
+  {
+    id: 15,
+    name: "Batuk berdarah",
+    value: 15,
+  },
+  {
+    id: 16,
+    name: "Rasa mual berlebihan",
+    value: 16,
+  },
+  {
+    id: 17,
+    name: "Ada rasa sakit di kepala (bukan pusing)",
+    value: 17,
+  },
+  {
+    id: 18,
+    name: "Warna kulit biru",
+    value: 18,
+  },
+  {
+    id: 19,
+    name: "Pembengkakan di kaki",
+    value: 19,
+  },
+  {
+    id: 20,
+    name: "Stress",
+    value: 20,
+  },
+  {
+    id: 21,
+    name: "Mudah lelas kalau sedikit beraktivitas",
+    value: 21,
+  },
+  {
+    id: 22,
+    name: "Kesusahan/kesulitan untuk berolahraga",
+    value: 22,
+  },
+  {
+    id: 23,
+    name: "Sering terbangun di malam hari",
+    value: 23,
+  },
+  {
+    id: 24,
+    name: "Ketika tidur menggunakan banyak bantal di are kepala",
+    value: 24,
+  },
+  {
+    id: 25,
+    name: "Kolestrol Tinggi",
+    value: 25,
+  },
+  {
+    id: 26,
+    name: "Batuk berlebihan",
+    value: 26,
+  },
+  {
+    id: 27,
+    name: "Tangan sering berkeringat",
+    value: 27,
+  },
+  {
+    id: 28,
+    name: "Sering kehabisan napas dimalam hari",
+    value: 28,
+  },
+  {
+    id: 29,
+    name: "Tidak nafsu makan",
+    value: 29,
+  },
+  {
+    id: 30,
+    name: "Dada terasa tidak nyaman",
+    value: 30,
+  },
+  {
+    id: 31,
+    name: "Napas terasa pendek ketika beraktivitas",
+    value: 31,
   },
 ];
 
--- src/data/dataPenyakit.ts
+++ src/data/dataPenyakit.ts
@@ -1,10 +1,36 @@
 const Penyakit = [
   {
     id: 1,
-    name: "Penyakit 1",
-    description: "Ini penyakit 1",
+    name: "Penyakit Jantung Koroner",
+    description: "Damage or disease in the heart's major blood vessels.",
+    detail: "Detail penyakit 1",
+  },
+  {
+    id: 2,
+    name: "Penyakit Jantung Bawaan (keturunan)",
+    description: "An abnormality in the heart that develops before birth",
+    detail: "Detail penyakit 1",
+  },
+  {
+    id: 3,
+    name: "Arrhythmia",
+    description:
+      "Improper beating of the heart, whether irregular, too fast or too slow.",
+    detail: "Detail penyakit 1",
+  },
+  {
+    id: 4,
+    name: "Penyakit katup jantung",
+    description:
+      "One or more of the valves in your heart doesn't work properly",
+    detail: "Detail penyakit 1",
+  },
+  {
+    id: 5,
+    name: "Gagal Jantung",
+    description:
+      "A chronic condition in which the heart doesn't pump blood as well as it should.",
     detail: "Detail penyakit 1",
   },
 ];
-
 export default Penyakit;
